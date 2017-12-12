32931725713ac242658d16656eff8f7e89657b12
Stephan Gieb <gieby@live.de>
2017-12-12 14:46:29 +0100

    + Skeleton für Bundle erstellt

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,28 @@
+<?php
+namespace yupdesign\AFKuenstler\ContaoManager;
+
+/**
+ * Use-Statements für das normale manager-Plugin, als auch für Routing
+ */
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class Plguin implements BundlePluginInterface, RoutingPluginInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getBundles(ParserInterface $parser)
+	{
+		return 
+		[
+			BundleConfig::create('yupdesin\AFKuenstler\yupdesignAFKuenstler')
+			->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle'])
+			->setReplace(['af']),
+		]
+	}
+}
---
+++ src/yupdesignAFKuenstler.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace yupdesign\AFKuenstler;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class yupdesignAFKuenstler extends Bundle
+{
+}
