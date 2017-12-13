86760005d74977543fbb31e7371ccecd075ea990
Stephan Gieb <stephan.gieb@live.com>
2017-12-13 15:47:19 +0100

    ~ Fix im Sprachfile

---
+++ src/Resources/contao/languages/de/tl_af_vitablock.php
@@ -0,0 +1,23 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_af_vitablock']['new'][0] = 'Neue Vita';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['new'][1] = 'Eine neue Vita anlegen';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['edit'][0] = 'Bearbeiten';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['edit'][1] = 'Vita bearbeiten';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['delete'][0] = 'Löschen';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['delete'][1] = 'Vita löschen';
+
+$GLOBALS['TL_LANG']['tl_af_vitablock']['entries'][0] = 'Einträge';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['date_from'][0] = 'Von';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['date_to'][0] = 'Bis';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['display_short'][0] = 'Kurzvita';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['display_long'][0] = 'Langvita';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['display_pdf'][0] = 'PDF';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['entry_text'][0] = 'Eintrag';
+
+$GLOBALS['TL_LANG']['tl_af_vitablock']['block_short'][0] = 'Kurzvita';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['block_short'][1] = 'Zeigt den entsprechenden Block in der jeweiligen Ausgabe an.';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['block_long'][0] = 'Langvita';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['block_long'][1] = 'Zeigt den entsprechenden Block in der jeweiligen Ausgabe an.';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['block_pdf'][0] = 'PDF';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['block_pdf'][1] = 'Zeigt den entsprechenden Block in der jeweiligen Ausgabe an.';
