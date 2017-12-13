6a7ae53310e08c5902e61385d39c6237d83ad581
Stephan Gieb <stephan.gieb@live.com>
2017-12-13 17:18:32 +0100

    ~ Sprachfiles

--- src/Resources/contao/languages/de/tl_af_vitablock.php
+++ src/Resources/contao/languages/de/tl_af_vitablock.php
@@ -7,7 +7,8 @@ $GLOBALS['TL_LANG']['tl_af_vitablock']['edit'][1] = 'Vita bearbeiten';
 $GLOBALS['TL_LANG']['tl_af_vitablock']['delete'][0] = 'Löschen';
 $GLOBALS['TL_LANG']['tl_af_vitablock']['delete'][1] = 'Vita löschen';
 
-$GLOBALS['TL_LANG']['tl_af_vitablock']['entries'][0] = 'Einträge';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['entries_legend'] = 'Einträge';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['entries'][0] = '';
 $GLOBALS['TL_LANG']['tl_af_vitablock']['date_from'][0] = 'Von';
 $GLOBALS['TL_LANG']['tl_af_vitablock']['date_to'][0] = 'Bis';
 $GLOBALS['TL_LANG']['tl_af_vitablock']['display_short'][0] = 'Kurzvita';
@@ -15,6 +16,11 @@ $GLOBALS['TL_LANG']['tl_af_vitablock']['display_long'][0] = 'Langvita';
 $GLOBALS['TL_LANG']['tl_af_vitablock']['display_pdf'][0] = 'PDF';
 $GLOBALS['TL_LANG']['tl_af_vitablock']['entry_text'][0] = 'Eintrag';
 
+$GLOBALS['TL_LANG']['tl_af_vitablock']['entry_legend'] = 'Block-Eigenschaften';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['title'][0] = 'Titel';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['title'][1] = 'Der Titel wird als Überschrift des jeweiligen Blocks angezeigt.';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['type'][0] = 'Typ';
+$GLOBALS['TL_LANG']['tl_af_vitablock']['type_ref'] = array('default' => 'Standard', 'af_exhibition' => 'Ausstellung bei art+form');
 $GLOBALS['TL_LANG']['tl_af_vitablock']['block_short'][0] = 'Kurzvita';
 $GLOBALS['TL_LANG']['tl_af_vitablock']['block_short'][1] = 'Zeigt den entsprechenden Block in der jeweiligen Ausgabe an.';
 $GLOBALS['TL_LANG']['tl_af_vitablock']['block_long'][0] = 'Langvita';
