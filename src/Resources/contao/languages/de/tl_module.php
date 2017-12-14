d217d4da381ae1b28d51517b57b43d01f86985db
Stephan Gieb <stephan.gieb@live.com>
2017-12-14 15:59:32 +0100

    ~ Sprachfiles

---
+++ src/Resources/contao/languages/de/tl_module.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['af_galerie'] = 'art+form';
+$GLOBALS['TL_LANG']['MOD']['keunstler'][0] = 'Künstler';
+$GLOBALS['TL_LANG']['MOD']['kuenstler'][1] = 'Künstler-Vitas verwalten';
