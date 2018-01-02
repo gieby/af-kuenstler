88d357aa9e52f20c10fc19c39b678cd0a7dceeac
Stephan Gieb <gieby@live.de>
2018-01-02 13:59:55 +0100

    ~ Benennung der Route vereinheitlicht

--- src/Controller/PDFController.php
+++ src/Controller/PDFController.php
@@ -5,20 +5,21 @@ namespace yupdesign\AFKuenstler\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PDFController extends Controller {
 
 	/**
-	 * @Route("/pdf/{firstname}-{lastname}", name="pdf", defaults={"_scope" = "frontend", "_token_check" = false})
+	 * @Route("/pdf/{firstname}-{lastname}", name="kuenstler_pdf", defaults={"_scope" = "frontend", "_token_check" = false})
 	 */
 	public function pdfAction($firstname='',$lastname='')
 	{
 		$this->container->get("contao.fromework")->initialize();
 
-		$controller = new \yupdesign\AFKuenstler\GeneratePDF();
+		//$controller = new \yupdesign\AFKuenstler\GeneratePDF();
 
 		if($firstname != '' && $lastname != '') {
-		//	$data = $controller->getPDF($firstname,$lastname);
+			$data = $controller->getPDF($firstname,$lastname);
 		}
 	}
 }
