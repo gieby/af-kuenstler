4de6a2bdf29eb7e83913cf57773295f2d3ff10f8
Stephan Gieb <gieby@live.de>
2018-01-03 11:55:26 +0100

    ..

--- src/Resources/contao/controllers/FrontendPDF.php
+++ src/Resources/contao/controllers/FrontendPDF.php
@@ -44,7 +44,7 @@ class FrontendPDF extends \Frontend
 
     $artist_fname = $objKuenstler->firstname;
     $artist_lname = $objKuenstler->lastname;
-    $artist_image = \FilesModel::findByUuid($this->singleSRC)->$path;
+    $artist_image = \FilesModel::findByUuid($this->profile_img)->$path;
 
     
 
