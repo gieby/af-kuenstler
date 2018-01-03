9857712b19766e96689f62bdf7b0cc64c147eda4
Stephan Gieb <gieby@live.de>
2018-01-03 15:15:55 +0100

    ~ Ränder

--- src/Resources/contao/controllers/FrontendPDF.php
+++ src/Resources/contao/controllers/FrontendPDF.php
@@ -78,6 +78,7 @@ class FrontendPDF extends \Frontend
     $pdf->SetAuthor('art+form');
     $pdf->SetTitle($artist_fname . ' '. $artist_lname . ' - Vita');
     $pdf->SetSubject('Vita für ' . $artist_fname . ' '. $artist_lname);
+    $pdf->SetMargins(29.7,14,14);
 
     $pdf->AddPage();
     $pdf->SetFont('Arial','B',16);
