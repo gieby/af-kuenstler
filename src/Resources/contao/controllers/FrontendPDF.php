9dcaca4f8c884734593d8ad4a47717992784bc6d
Stephan Gieb <gieby@live.de>
2018-01-03 14:18:43 +0100

    .

--- src/Resources/contao/controllers/FrontendPDF.php
+++ src/Resources/contao/controllers/FrontendPDF.php
@@ -90,8 +90,7 @@ class FrontendPDF extends \Frontend
     }
 
     foreach ($artist_blocks as $title => $entries) {
-        $pdf->displayBasicTable($title,$entries);
-        $pdf->Write(0,implode(',',$entries));
+        $pdf->displayBasicTable($title, $entries);
     }
 
     // fertige PDF als String zurück an den Controller schicken
--- src/VitaPDF.php
+++ src/VitaPDF.php
@@ -18,15 +18,14 @@ class VitaPDF extends \FPDF {
 	public function displayBasicTable($header='', $data) {
 		$this->setFont('Arial','B',10);
 		$this->Write(10,$header);
+		$this->setFont('Arial','',10);
 		$this->Ln();
-		$this->setFont('');
-
+		
 		foreach ($data as $row) {
-			$this->Write(0,implode(',',$row));
 			$year = $row[0];
 			$entry = $row[1];
 			$this->Cell(40,7,$year);
-			$this->Cell(0,7,$entry);
+			$this->Cell(0,7,utf8_decode($entry));
 			$this->Ln();
 		}
 	}
