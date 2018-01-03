1d306810f512da039a312d2c7fddd01fd8edbe3d
Stephan Gieb <gieby@live.de>
2018-01-03 14:25:48 +0100

    ..

--- src/VitaPDF.php
+++ src/VitaPDF.php
@@ -15,7 +15,7 @@ class VitaPDF extends \FPDF {
 
 	}
 
-	public function displayBasicTable($header='', $data) {
+	public function displayBasicTable($header='',$data) {
 		$this->setFont('Arial','B',10);
 		$this->Write(10,$header);
 		$this->setFont('Arial','',10);
@@ -24,9 +24,14 @@ class VitaPDF extends \FPDF {
 		foreach ($data as $row) {
 			$year = $row[0];
 			$entry = $row[5];
+			
+			$this->setFont('Arial','',10);
 			$this->Cell(40,7,$year);
+			$this->setFont('Arial','',10);
 			$this->Cell(0,7,utf8_decode($entry));
 			$this->Ln();
+
+			$this->Write(7,utf8_decode(implode(',',$row)));
 		}
 	}
 
