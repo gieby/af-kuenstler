46d23acef8b8a99f404c4c34741def246dd581fe
Stephan Gieb <gieby@live.de>
2018-01-04 10:07:49 +0100

    ..

--- src/VitaPDF.php
+++ src/VitaPDF.php
@@ -15,7 +15,7 @@ class VitaPDF extends \FPDF {
 		$this->SetY(-15);
 		$this->SetX(21.1);
 		$this->SetFillColor(125);
-		$this->Rect($this->GetX(),$this->GetY(),35,10,'F');
+		$this->Rect(0,0,35,10,'F');
 		$this->Rect($this->GetX()+5,$this->GetY(),0,10,'F');
 	}
 
