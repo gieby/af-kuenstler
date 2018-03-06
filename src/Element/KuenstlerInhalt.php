7b80315406e8ebcbcaa77f00aa31bcd1359d4ddc
Stephan Gieb <gieby@live.de>
2018-03-06 12:19:49 +0100

    ..

--- src/Element/KuenstlerInhalt.php
+++ src/Element/KuenstlerInhalt.php
@@ -99,7 +99,7 @@ class KuenstlerInhalt extends \ContentElement
      foreach ($block as $entry) {
        $exhib = $exhibDB->execute($entry['exhib_id'])->next();
 
-       $exhib_url = \PageModel::findByPk($exhib->exhib_page)->getFrontendUrl();
+       //$exhib_url = \PageModel::findByPk($exhib->exhib_page)->getFrontendUrl();
 
         $data = array(
           'date' => date('Y',$exhib->date),
