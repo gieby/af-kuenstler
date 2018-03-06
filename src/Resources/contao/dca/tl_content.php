bc9390904b86e3674556949cdbbe904798b2c8f9
Stephan Gieb <gieby@live.de>
2018-03-06 14:05:15 +0100

    ..

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -7,7 +7,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['kuenstler']	= array
 	'label'             	=> &$GLOBALS['TL_LANG']['tl_content']['kuenstler'],
     'exclude'           => true,
     'inputType'         => 'select',
-    'options_callback'  => array('\VitaBlock', 'getKuenstlerIDs'),
+    'options_callback'  => array('yupdesign\AFKuenstler\VitaBlock', 'getKuenstlerIDs'),
     'eval'              => array('mandatory' => true, 'includeBlankOption' => true,'tl_class' => 'w50'),
     'sql'               => "int(10) unsigned NOT NULL default '0'"
 );
